8970e59f02c424bbb01e54d05719f3fa36a7316b
Soumil Uppal <uppals@purdue.edu>
2018-04-10 02:53:40 -0400

    Game complete for one computer

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4 {
 		this.COLUMNS = 7;
 		this.player = 'red';
 		this.selector = selector;
+		this.over = false;
 		this.createGrid();
 		this.setupEventListeners();
 	}
@@ -41,6 +42,7 @@ class Connect4 {
 		}
 
 		$board.on('mouseenter', '.col.empty', function() {
+			if(that.over) return;
 			const col = $(this).data('col');
 			const $lastEmptyCell = findLastEmptyCell(col);
 			$lastEmptyCell.addClass(`next-${that.player}`);
@@ -52,13 +54,74 @@ class Connect4 {
 
 		$board.on('click', '.col.empty', function() {
 			const col = $(this).data('col');
+			const row = $(this).data('row');
 			const $lastEmptyCell = findLastEmptyCell(col);
 			$lastEmptyCell.removeClass(`empty next-${that.player}`);
 			$lastEmptyCell.removeClass('empty');
 			$lastEmptyCell.addClass(that.player);
+			$lastEmptyCell.data('player', that.player);
+
+			const winner = that.winnerCheck($lastEmptyCell.data('row'), $lastEmptyCell.data('col'));
+			if(winner) {
+				that.over = true;
+				alert(`Game Over! Player ${that.player} has won!`);
+				$('.col.empty').removeClass('empty');
+				return;
+			}
+
 			that.player = (that.player === 'red') ? 'yellow' : 'red';
 			$(this).trigger('mouseenter')
 		});
+	}
+
+	winnerCheck(row, col) {
+		const that = this;
+
+		function $getCell(row, col) {
+			return $(`.col[data-row='${row}'][data-col='${col}']`);
+		}
+
+		function checkDir(dir) {
+			let total = 0;
+			let i = row + dir.i;
+			let j = col + dir.j;
+			let $next = $getCell(i, j);
+
+			while(i >= 0 && i < that.ROWS && j >= 0 && j < that.COLUMNS && $next.data('player') === that.player) {
+				total++;
+				i += dir.i;
+				j += dir.j;
+				$next = $getCell(i, j);
+			}
+			return total;
+		}
+
+		function check(a, b) {
+			const total = 1 + checkDir(a) + checkDir(b);
+			if(total >= 4) {
+				return that.player;
+			}
+			else {
+				return null;
+			}
+		}
+
+		function diagonalbltotr() {
+			return check({i: 1, j: -1}, {i: 1, j: 1});
+		}
+
+		function diagonaltltobr() {
+			return check({i: 1, j: 1}, {i: -1, j: -1});
+		}
+
+		function vertical() {
+			return check({i: -1, j: 0}, {i: 1, j: 0});
+		}
+
+		function horizontal() {
+			return check({i: 0, j: -1}, {i: 0, j: 1});
+		}
+		return vertical() || horizontal() || diagonalbltotr();
 	}
 
 }
