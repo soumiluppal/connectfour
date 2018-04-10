456c0d49fc14c64fba34c1e9225c89a184b273d0
Soumil Uppal <uppals@purdue.edu>
2018-04-09 23:36:34 -0400

    Added tokens addition

--- connect4.js
+++ connect4.js
@@ -2,8 +2,10 @@ class Connect4 {
 	constructor(selector) {
 		this.ROWS = 6;
 		this.COLUMNS = 7;
+		this.player = 'red';
 		this.selector = selector;
 		this.createGrid();
+		this.setupEventListeners();
 	}
 	createGrid() {
 		const $board = $(this.selector);
@@ -13,12 +15,49 @@ class Connect4 {
 				.addClass('row');
 			for(let col = 0; col < this.COLUMNS; col++) {
 				const $col = $('<div>')
-					.addClass('col empty');
+					.addClass('col empty')
+					.attr('data-col', col)
+					.attr('data-row', row);
 				$row.append($col);
 			}
 			$board.append($row);
 		}
 		console.log($board.html());
+	}
+
+	setupEventListeners() {
+		const $board = $(this.selector);
+		const that = this;
+
+		function findLastEmptyCell(col) {
+			const cells = $(`.col[data-col = '${col}']`);
+			for(let i = cells.length - 1; i >= 0; i--) {
+				const $cell = $(cells[i]);
+				if($cell.hasClass('empty')) {
+					return $cell;
+				}
+			}
+			return null;
+		}
+
+		$board.on('mouseenter', '.col.empty', function() {
+			const col = $(this).data('col');
+			const $lastEmptyCell = findLastEmptyCell(col);
+			$lastEmptyCell.addClass(`next-${that.player}`);
+		});
+
+		$board.on('mouseleave', '.col', function() {
+			$('.col').removeClass(`next-${that.player}`);
+		});
+
+		$board.on('click', '.col.empty', function() {
+			const col = $(this).data('col');
+			const row = $(this).data('row');
+			const $lastEmptyCell = findLastEmptyCell(col);
+			$lastEmptyCell.removeClass('empty');
+			$lastEmptyCell.addClass(that.player);
+			that.player = (that.player === 'red') ? 'yellow' : 'red';
+		});
 	}
 
 }
