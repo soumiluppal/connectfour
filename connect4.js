0363bd1538216c0c93fb5dff5317a8bb89b14ab3
Soumil Uppal <uppals@purdue.edu>
2018-04-12 12:13:37 -0400

    ttry 22

--- connect4.js
+++ connect4.js
@@ -69,7 +69,6 @@ class Connect4 {
 			$lastEmptyCell.data('player', that.player);
 			that.sendMsg(col, socket);
 			document.getElementById('which').style.backgroundColor = that.player;
-			document.getElementById('turn').innerHTML = this.getParameterByName('user2', window.location.href);
 			const winner = that.winnerCheck($lastEmptyCell.data('row'), $lastEmptyCell.data('col'));
 			if(winner) {
 				that.over = true;
